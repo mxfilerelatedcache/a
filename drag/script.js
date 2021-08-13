d96ca26ba03e9d7eaa036f7adca97d00b81a8d61
Simon <info@simonkrukowski.de>
2021-08-13 16:45:16 +0200

    sfvfsv

--- drag/script.js
+++ drag/script.js
@@ -47,22 +47,8 @@ function dragMoveListener (event) {
 // this function is used later in the resizing and gesture demos
 window.dragMoveListener = dragMoveListener
 
-const $body = document.querySelector('body');
-let scrollPosition = 0;
-
-export default {
-  enable() {
-    scrollPosition = window.pageYOffset;
-    $body.style.overflow = 'hidden';
-    $body.style.position = 'fixed';
-    $body.style.top = `-${scrollPosition}px`;
-    $body.style.width = '100%';
-  },
-  disable() {
-    $body.style.removeProperty('overflow');
-    $body.style.removeProperty('position');
-    $body.style.removeProperty('top');
-    $body.style.removeProperty('width');
-    window.scrollTo(0, scrollPosition);
-  }
+function preventBehavior(e) {
+  e.preventDefault(); 
 };
+
+document.addEventListener("touchmove", preventBehavior, {passive: false});
